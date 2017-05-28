bd8898ee861d18b34052c2b48aef02b75ea4124f
Giovanni <giomele27@gmail.com>
2017-05-28 16:57:14 +0200

    familySet...

--- src/main/java/it/polimi/ingsw/ps29/model/game/familymember/FamilySet.java
+++ src/main/java/it/polimi/ingsw/ps29/model/game/familymember/FamilySet.java
@@ -1,5 +1,14 @@
 package it.polimi.ingsw.ps29.model.game.familymember;
 
+import java.util.HashMap;
+
+import it.polimi.ingsw.ps29.model.game.resources.Resource;
+
 public class FamilySet {
 
+	private HashMap <String, FamilyMember> familyMembers;
+	
+	public FamilyMember getFamilyMember(String member){
+		return familyMembers.get(member);
+	}
 }
