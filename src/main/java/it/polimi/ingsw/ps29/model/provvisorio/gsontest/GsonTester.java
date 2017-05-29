f5b3a2b3b11f0d14ee3cad8a7f5da51ee66f9ded
JayAves <10491388@polimi.it>
2017-05-29 19:09:37 +0200

    ..

--- src/main/java/it/polimi/ingsw/ps29/model/provvisorio/gsontest/GsonTester.java
+++ src/main/java/it/polimi/ingsw/ps29/model/provvisorio/gsontest/GsonTester.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.ps29.model.provvisorio.gsontest;
 
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
+
+import it.polimi.ingsw.ps29.model.cards.effects.GainResourcesEffect;
+import it.polimi.ingsw.ps29.model.game.resources.Resource;
 
 public class GsonTester {
 	
@@ -9,4 +14,16 @@ public class GsonTester {
 	
 	JsonParser parser= new JsonParser();
 	
+	ArrayList<Resource> res = new ArrayList<Resource>();
+	
+	GainResourcesEffect immediate= new GainResourcesEffect(res);
+	
+	public void addResource(String type, int value) {
+		res.add(new Resource(type,value));
+	}
+	
+	
+	public static void main() {
+		
+	}
 }
