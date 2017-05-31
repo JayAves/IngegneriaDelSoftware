7d2b5f194538cb0ab6f559ebac2495d738da4961
JayAves <10491388@polimi.it>
2017-05-31 23:05:53 +0200

    Bozza lettura da file

---
+++ src/main/java/it/polimi/ingsw/ps29/model/provvisorio/gsontest/GsonConverter.java
@@ -0,0 +1,67 @@
+package it.polimi.ingsw.ps29.model.provvisorio.gsontest;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import it.polimi.ingsw.ps29.model.cards.Card;
+
+public class GsonConverter {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		final String JSON_PATH = "/home/jaycaves/Desktop/prova.json";
+
+		
+		
+		try {
+			Writer writer = new FileWriter(JSON_PATH);
+			Gson gson = new GsonBuilder().create();
+		    //gson.toJson(users, writer);
+			writer.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader br = new BufferedReader(new FileReader(JSON_PATH));
+		JsonParser parser = new JsonParser();
+		JsonArray array = parser.parse(br).getAsJsonArray();
+		
+		ArrayList<JsonObject> result = new ArrayList<>(array.size());
+
+	    for (int i=0; i < array.size(); i++) {
+	       
+	    	result.add((JsonObject) array.get(i));
+	    }
+	    
+	    for(JsonObject obj: result ) {
+	    	
+	    	//ciclo di trasformazione json object to Card
+	    }
+	}
+	
+
+	private Card getSingleCard() {
+		return null;
+	}
+
+	/* Codice extra
+	 * try {
+		JsonWriter  writer = new JsonWriter(new FileWriter(JSON_PATH));
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}*/
+
+}
