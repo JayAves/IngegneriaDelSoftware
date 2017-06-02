8ef63ee36723b4225163acf6fb79903a594739a9
JayAves <10491388@polimi.it>
2017-06-02 15:02:12 +0200

    aggiornamento mazzo

--- src/main/java/it/polimi/ingsw/ps29/model/provvisorio/gsontest/CardAdapter.java
+++ src/main/java/it/polimi/ingsw/ps29/model/provvisorio/gsontest/CardAdapter.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Type;
+import java.util.Collection;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -16,6 +17,7 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
 
 import it.polimi.ingsw.ps29.model.cards.Card;
 import it.polimi.ingsw.ps29.model.cards.effects.Effect;
@@ -57,16 +59,15 @@ public class CardAdapter implements JsonSerializer<Card>, JsonDeserializer<Card>
 		//adapter.serialize(card, null, null);
 		
 		
-		BufferedReader cards = new BufferedReader(new FileReader("/home/jaycaves/Desktop/prova.json"));
+		BufferedReader cards = new BufferedReader(new FileReader("src/main/java/cards.json"));
 	    GsonBuilder gcards = new GsonBuilder();
 	    gcards.registerTypeAdapter(Card.class, new CardAdapter());
 	    gcards.registerTypeAdapter(Effect.class, new EffectAdapter());
 	    gcards.registerTypeAdapter(Resource.class, new ResourceAdapter());
-	    Card cardz = gcards.create().fromJson(cards, Card.class);
-	    System.out.println(cardz.toString());
-	    if (cardz.getPeriod().equals(Period.FIRST)) {
-	    	System.out.println("\n true");
-	    }
+	    Card[] cardz = gcards.create().fromJson(cards, Card[].class);
+	    cardz[1].getType().toString();
+	    System.out.println(cardz[1].getType().toString());
+	    
 	    
 	}
 }
