c00d292b5e5018bb298ddb822f6e1d4ab12d4aaf
Giovanni <giomele27@gmail.com>
2017-06-04 07:19:10 +0200

    carte + nuovo effetto

---
+++ src/main/java/it/polimi/ingsw/ps29/model/cards/effects/MultipliedResourcesForResourceEffect.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.ps29.model.cards.effects;
+
+import it.polimi.ingsw.ps29.model.game.PersonalBoard;
+import it.polimi.ingsw.ps29.model.game.Player;
+import it.polimi.ingsw.ps29.model.game.resources.Container;
+import it.polimi.ingsw.ps29.model.game.resources.Resource;
+
+public class MultipliedResourcesForResourceEffect extends Effect{
+	
+	private String multiplied;
+	private double amount;
+	private String gained;
+
+	public MultipliedResourcesForResourceEffect(String multiplied, String gained, double amount) {
+		super();
+		this.multiplied = multiplied;
+		this.gained = gained;
+		this.amount = amount;
+	}
+
+	@Override
+	public void performEffect(Player player) {
+		PersonalBoard board = player.getPersonalBoard();
+		Container resources = board.getResources();
+		int multiplier = (int) (resources.getResource(multiplied).getAmount()/amount);
+		//int multiplier = board.getCards(cardType).size();
+		resources.updateResource(new Resource(gained, multiplier));
+		
+	}
+
+}
