7b92631f4abeb5a2e92124766da362ade2307422
Giovanni <giomele27@gmail.com>
2017-06-13 16:54:48 +0200

    Leader Card

--- src/main/java/it/polimi/ingsw/ps29/model/cards/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/ps29/model/cards/LeaderCard.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw.ps29.model.cards;
 
-public class LeaderCard {
+import java.util.ArrayList;
 
+import it.polimi.ingsw.ps29.model.cards.effects.Effect;
+import it.polimi.ingsw.ps29.model.game.resources.Container;
+import it.polimi.ingsw.ps29.model.game.resources.Resource;
+
+public class LeaderCard {
+	
+	private String name;
+	private Effect leaderEffect;
+	private Container requirements;
+	
+    public LeaderCard ( String name, Effect effect, ArrayList<Resource> requirements){
+    	this.name = name;
+    	this.leaderEffect = effect;
+    	for (Resource res : requirements){
+    		requirements.add(res);
+    	}
+    }
 }
