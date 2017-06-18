52d73778915385da224ed2d27010dc8a232a54f9
Giovanni <giomele27@gmail.com>
2017-06-18 11:53:06 +0200

    leadercard e family

--- src/main/java/it/polimi/ingsw/ps29/model/cards/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/ps29/model/cards/LeaderCard.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.ps29.model.cards;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import it.polimi.ingsw.ps29.model.cards.effects.Effect;
 import it.polimi.ingsw.ps29.model.game.resources.Container;
@@ -11,12 +12,21 @@ public class LeaderCard {
 	private String name;
 	private Effect leaderEffect;
 	private Container requirements;
+	private HashMap <String, Integer> cardRequirements;
 	
-    public LeaderCard ( String name, Effect effect, ArrayList<Resource> requirements){
+    public LeaderCard ( String name, Effect effect, ArrayList<Resource> requirements, ArrayList<Integer> cardRequirements){
     	this.name = name;
     	this.leaderEffect = effect;
     	for (Resource res : requirements){
     		requirements.add(res);
     	}
+    	if (cardRequirements.get(0) > 0)
+    		this.cardRequirements.put("territory", cardRequirements.get(0));
+    	if (cardRequirements.get(1) > 0)
+    		this.cardRequirements.put("Building", cardRequirements.get(0));
+    	if (cardRequirements.get(2) > 0)
+    		this.cardRequirements.put("character", cardRequirements.get(0));
+    	if (cardRequirements.get(3) > 0)
+    		this.cardRequirements.put("venture", cardRequirements.get(0));
     }
 }
---
+++ src/main/java/it/polimi/ingsw/ps29/model/game/familymember/Family.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.ps29.model.game.familymember;
+
+import java.util.HashMap;
+
+import it.polimi.ingsw.ps29.model.game.Color;
+import it.polimi.ingsw.ps29.model.game.DiceColor;
+
+public class Family {
+	
+	private HashMap <DiceColor, FamilyMemberInterface> familyMembers;
+	private FakeFamilyMemberInterface fakeFamiltMember;
+	
+	public Family(Color playerColor){
+		
+		familyMembers.put(DiceColor.NEUTRAL, new FamilyMember(DiceColor.NEUTRAL, playerColor));
+		familyMembers.put(DiceColor.BLACK, new FamilyMember(DiceColor.BLACK, playerColor));
+		familyMembers.put(DiceColor.WHITE, new FamilyMember(DiceColor.WHITE, playerColor));
+		familyMembers.put(DiceColor.ORANGE, new FamilyMember(DiceColor.ORANGE, playerColor));
+	}
+
+}
