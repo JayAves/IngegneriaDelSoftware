56238faf486ae9cdd5bdf019614a3c27c8799b65
Giovanni <giomele27@gmail.com>
2017-06-24 11:51:28 +0200

    ludovico ariosto

---
+++ src/main/java/it/polimi/ingsw/ps29/model/cards/effects/FixFamilyMemberPowerByColor.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.ps29.model.cards.effects;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.ps29.model.game.DiceColor;
+import it.polimi.ingsw.ps29.model.game.Player;
+
+public class FixFamilyMemberPowerByColor extends EmpowermentActionEffect{
+	
+	private ArrayList<String> colors;
+	
+	public FixFamilyMemberPowerByColor(ArrayList<String> colors, int diceEmpowerment) {
+		
+		this.colors = colors;
+		this.diceEmpowerment = diceEmpowerment;
+	}
+	
+	@Override
+	public void performEffect(Player player) {
+		
+		for (String color: colors){
+			player.getFamiliarByColor(DiceColor.valueOf(color)).setFixedPower();
+			player.getFamiliarByColor(DiceColor.valueOf(color)).setPower(diceEmpowerment);
+		}
+				
+	}
+	
+}
