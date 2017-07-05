553f065a53c5965adaac255cb6c1747fdb6d9ed6
Giovanni <giomele27@gmail.com>
2017-07-06 00:16:41 +0200

    fix test

--- src/test/java/it/polimi/ingsw/ps29/HarvestActionTest.java
+++ src/test/java/it/polimi/ingsw/ps29/HarvestActionTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import it.polimi.ingsw.ps29.controller.ChoiceToMove;
 import it.polimi.ingsw.ps29.messages.ActionChoice;
 import it.polimi.ingsw.ps29.messages.exception.RejectException;
+import it.polimi.ingsw.ps29.messages.exception.SpaceOccupiedException;
 import it.polimi.ingsw.ps29.model.action.HarvestAction;
 import it.polimi.ingsw.ps29.model.game.DiceColor;
 import it.polimi.ingsw.ps29.model.game.Match;
@@ -29,13 +30,15 @@ public class HarvestActionTest extends TestCase{
 		
 		String player1 = "primo";
 		String player2 = "secondo";
-		ArrayList<String> names= new ArrayList<String>();
+		String player3 = "terzo";
+ 		ArrayList<String> names= new ArrayList<String>();
 		names.add(player1);
 		names.add(player2);
 		
 		model = new Match(names);
 		
 		space = (ActivityArea) model.getBoard().getSpace("Harvest");
+		System.out.println(space);
 		
 		model.getBoard().getPlayerByName("primo").getFamiliarByColor(DiceColor.BLACK).setPower(4);
 		model.getBoard().getPlayerByName("secondo").getFamiliarByColor(DiceColor.BLACK).setPower(2);
@@ -49,25 +52,38 @@ public class HarvestActionTest extends TestCase{
 		
 			
 		HarvestAction action = new HarvestAction(model, move);
+		//System.out.println(model.getBoard().getSpace("harvest"));
 		action.actionHandler();
+		//System.out.println(model.getBoard().getSpace("harvest"));
+		System.out.println(model.getBoard().getSpace("Harvest"));
 		
 		ActionChoice aChoiceSecond = new ActionChoice("secondo");
-		aChoiceFirst.setChoice(0, 1);
-		aChoiceFirst.setChoice(3, 1);
+		aChoiceSecond.setChoice(0, 1);
+		aChoiceSecond.setChoice(3, 1);
 		
 		Move moveSecond = ChoiceToMove.createMove(aChoiceSecond, model.getBoard());
 		
 		scndAction = new HarvestAction(model, moveSecond);
+		//scndAction.actionHandler();
 		
 	}
 	
 
-	@Test(expected = RejectException.class)
-	public void test() {
+	@Test(expected = SpaceOccupiedException.class)
+	public void test(){
 		//fail("Not yet implemented");
 		assertEquals(true, model.getBoard().getPlayerByName("primo").getFamiliarByColor(DiceColor.BLACK).getBusy());
 		assertEquals(true, space.getClosed());
-	   
+		try {
+			scndAction.isPlaceable();
+			fail ("expected SpaceOccupiedException");
+		} catch (RejectException e) {
+			// TODO Auto-generated catch block
+		}
+		
+		
+		assertEquals(false, model.getBoard().getPlayerByName("secondo").getFamiliarByColor(DiceColor.BLACK).getBusy());
+			
 	}
 
 }
